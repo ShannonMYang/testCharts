a449a7b1238e4e1d96c85134702a9b67a8350a16
杨秀坤 <myang.shannon1122@gmail.com>
2018-04-19 12:13:27 +0800

    Initial Commit

---
+++ testCharts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCharts
+//
+//  Created by Shannon MYang on 2018/4/19.
+//  Copyright © 2018年 Shannon MYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testCharts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testCharts
+//
+//  Created by Shannon MYang on 2018/4/19.
+//  Copyright © 2018年 Shannon MYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
